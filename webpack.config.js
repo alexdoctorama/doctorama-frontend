c52cc148f4ccbc967542ed01f1f2034909e58509
alex <a.doukas@que-tech.com>
2024-03-11 00:08:14 +0200

    Changed host

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,11 @@ module.exports = {
   devServer: {
     port: 3001,
     historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+    },
     onAfterSetupMiddleware() {
       opn(`http://localhost:${this.port}/react/template/index`);
     },
